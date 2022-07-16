b76957ec5ce97a183a6363c86ef8877452d70cc9
Victor Inojosa <vijoin@gmail.com>
2022-07-16 11:33:29 -0300

    Read text from stdin and print it back

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Please, type your name: ");
+
+    let mut name: String = String::new();
+
+    std::io::stdin().read_line(&mut name).unwrap();
+
+    println!("Hola, bienvenid@: {}", name);
 }
