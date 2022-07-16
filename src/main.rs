6c088a65876e4b9799d0e27a2017e359a6b5b40d
Victor Inojosa <vijoin@gmail.com>
2022-07-16 11:44:32 -0300

    Ask for name and age and print it out
    * convert string to integer
    * trim newline left by read_line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
     println!("Please, type your name: ");
-
     let mut name: String = String::new();
-
     std::io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
 
-    println!("Hola, bienvenid@: {}", name);
+    println!("Good! Now please, type your age: ");
+    let mut age: String = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+
+    // convert to integer
+    let age_int: u8 = age.trim().parse().unwrap();
+
+    println!("Hi!, welcome {} of {} years old", name, age_int);
 }
